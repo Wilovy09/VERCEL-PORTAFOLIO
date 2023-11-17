f154ffe195dd14559b7492d5cf018009f002e3ec
Wilovy09 <SrWilovy09@gmail.com>
2023-11-17 05:41:36 -0600

    BASE

---
+++ vercel_build_staticfiles.sh
@@ -0,0 +1,11 @@
+# build_files.sh
+echo "Instalando requerimientos..."
+pip install -r requirements.txt
+
+# Automatic migrations
+#echo "Iniciando migraciones..."
+#python manage.py makemigrations && python manage.py migrate
+
+# Colecta de staticsFiles en theme/static/
+echo "Colectando StaticFiles..."
+python3.9 manage.py collectstatic --noinput
