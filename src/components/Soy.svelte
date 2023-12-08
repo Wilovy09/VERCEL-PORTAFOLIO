a28d8fb2062383c314c7d7511a0d5d26684c239b
Wilovy09 <SrWilovy09@gmail.com>
2023-12-08 13:53:56 -0600

    update index image

--- src/components/Soy.svelte
+++ src/components/Soy.svelte
@@ -1,7 +1,7 @@
 <div class="">
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
-        <div class="pt-14 px-4 flex justify-center md:justify-end">
-            <img class="md:max-w-md lg:max-w-md rounded-xl" src="/profile1.webp" alt="Imagen1">
+    <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2">
+        <div class="pt-14 px-4 flex justify-center">
+            <img class="sm:max-w-sm md:max-w-md lg:max-w-md rounded-xl" src="/profile1.webp" alt="Imagen1">
         </div>
         <div class="text-center pt-4 md:my-auto mb-44">
             <h1 class="font-bold text-2xl mb-2 mt-12">Soy <i>Emiliano Maldonado G.</i></h1>
