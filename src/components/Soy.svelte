9cec7bdf3b5abc8d2754b400bc0ce086609bcdae
Wilovy09 <SrWilovy09@gmail.com>
2023-12-08 14:06:34 -0600

    margins on index text

--- src/components/Soy.svelte
+++ src/components/Soy.svelte
@@ -1,9 +1,9 @@
 <div class="">
     <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2">
-        <div class="pt-14 px-4 flex justify-center">
+        <div class="pt-14 px-4 flex justify-center lg:justify-end">
             <img class="sm:max-w-sm md:max-w-md lg:max-w-md rounded-xl" src="/profile1.webp" alt="Imagen1">
         </div>
-        <div class="text-center pt-4 md:my-auto mb-44">
+        <div class="text-center pt-4 md:my-auto mb-44 xl:-ml-56">
             <h1 class="font-bold text-2xl mb-2 mt-12">Soy <i>Emiliano Maldonado G.</i></h1>
             <p class="text-xl mb-2">Un desarrollador autodidacta 🇲🇽.</p>
         </div>
