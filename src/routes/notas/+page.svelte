37930f3ca740d141d579e7d52c074146c7b3c28a
Wilovy09 <SrWilovy09@gmail.com>
2023-12-08 22:09:15 -0600

    Update after AforDigital saw it

--- src/routes/notas/+page.svelte
+++ src/routes/notas/+page.svelte
@@ -5,7 +5,7 @@
 
 <Layout>
     <h2 class="text-[#26bb80] text-5xl font-bold py-12 uppercase text-center">Mis Notas</h2>
-    <section class="min-h-screen">
+    <section class="min-h-screen overflow-hidden">
         <LinkNotas title="Prisma" link="https://github.com/Wilovy09/Prisma-Curso-ORM/blob/main/README.md"/>
         <LinkNotas title="Express & Prisma" link="https://github.com/Wilovy09/NodeJS_Prisma_API/blob/main/README.md"/>
     </section>
