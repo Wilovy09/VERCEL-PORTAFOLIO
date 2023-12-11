c2e6427e214608b74621a3d60ea66f098452da58
Wilovy09 <SrWilovy09@gmail.com>
2023-12-10 20:07:44 -0600

    add link target blank component

--- src/components/notas/BlankLinkNotas.svelte
+++ src/components/notas/BlankLinkNotas.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <h4 class="text-3xl font-bold py-6 uppercase text-center hover:scale-110 transition ease-in-out">
-    <a href="{link}">
+    <a href="{link}" target="_blank">
         {title}
     </a>
 </h4>
